fa0e000cb8cec494297af037aa6d64a1b26c4517
milk1000cc <info@milk1000.cc>
2014-10-27 18:29:04 +0900

    Add dayone-cli

---
+++ Casks/dayone-cli.rb
@@ -0,0 +1,10 @@
+class DayoneCli < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://dayoneapp.com/downloads/dayone-cli.pkg'
+  homepage 'http://dayoneapp.com/tools/cli-man/'
+  license :closed
+
+  pkg 'dayone-cli.pkg'
+end
