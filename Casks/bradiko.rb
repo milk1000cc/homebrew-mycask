e3f6a6b76921d530fbfc45eacc4c1af11013db86
milk1000cc <info@milk1000.cc>
2014-10-27 19:21:55 +0900

    Add Bradiko

---
+++ Casks/bradiko.rb
@@ -0,0 +1,10 @@
+class Bradiko < Cask
+  version '0.92.05'
+  sha256 'b9cca8051dd984b551c2b98ac1ef66d49635675490e79f43bde4d580c111a27e'
+
+  url "http://buimemo.artman21.net/buimemo/radiko/BRadiko.#{ version }.zip"
+  homepage 'http://www.ne.jp/asahi/mac/bui/radiko/'
+  license :gratis
+
+  app 'BRadiko.app'
+end
