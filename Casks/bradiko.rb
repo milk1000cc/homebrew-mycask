d0ace4e9bb95a1f40ae550408925f5f70002c4da
milk1000cc <info@milk1000.cc>
2015-10-17 11:03:50 +0900

    Update bradiko

--- Casks/bradiko.rb
+++ Casks/bradiko.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'bradiko' do
-  version '0.95.07'
-  sha256 'da3b21d92bbf5bfea187c17c53d5037516b924cb08ce04c97940ce2c82ab5bf0'
+  version '0.95.08'
+  sha256 'bea7516b80e09927f6d0e73b8c4af177054d764d0e355da1b2e79851b1ab8b58'
 
   url "http://buimemo.artman21.co.jp/buimemo/radiko/BRadiko.#{ version }.zip"
   homepage 'http://www.ne.jp/asahi/mac/bui/radiko/'
