f3a6033f9a19c8c4f4cee8a1b3871f0be21baa75
milk1000cc <info@milk1000.cc>
2018-08-03 11:21:48 +0900

    Remove dayone-cli

--- Casks/dayone-cli.rb
+++
@@ -1,9 +0,0 @@
-cask :v1 => 'dayone-cli' do
-  version :latest
-  sha256 :no_check
-
-  url 'http://dayoneapp.com/downloads/dayone-cli.pkg'
-  homepage 'http://dayoneapp.com/tools/cli-man/'
-
-  pkg 'dayone-cli.pkg'
-end
